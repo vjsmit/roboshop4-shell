ed8626ff80e3a06609f3d3871f9ca3195b37f9b4
smvijay <vj.smit97@gmail.com>
2024-03-16 07:17:32 -0400

    setup rabbitmq component

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,23 +6,23 @@ if [ -z "$rabbitmq_pwd" ]; then
 fi
 
 echo -e "${color}Setup erlang repo${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash   &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Setup rabbitmq repo${nocolor}"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash    &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Install rabbitmq${nocolor}"
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y    &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Start rabbitmq service${nocolor}"
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+systemctl enable rabbitmq-server    &>>${logfile}
+systemctl restart rabbitmq-server   &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Create app user and fix permissions${nocolor}"
-rabbitmqctl add_user roboshop ${rabbitmq_pwd}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop ${rabbitmq_pwd}   &>>${logfile}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"    &>>${logfile}
 func_StatCheck $?
