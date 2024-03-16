8f9f9072bb0f6d8b8069e5b2cddc1e35b530879d
smvijay <vj.smit97@gmail.com>
2024-03-16 02:14:49 -0400

    Modifying frontend

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
 source common.sh
 
 echo -e "${color}Installing Nginx${nocolor}"
-dnf install nginx -y
-func_StatCheck
+dnf install nginx -y    &>>${logfile}
+func_StatCheck $?
 
 echo -e "${color}Removing Default Dir${nocolor}"
-rm -rf /usr/share/nginx/html/*
-func_StatCheck
+rm -rf /usr/share/nginx/html/*    &>>${logfile}
+func_StatCheck $?
 
 echo -e "${color}Download frontend content${nocolor}"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-func_StatCheck
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip    &>>${logfile}
+func_StatCheck $?
 
 echo -e "${color}Unzip frontend content${nocolor}"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-func_StatCheck
+unzip /tmp/frontend.zip     &>>${logfile}
+func_StatCheck $?
 
 echo -e "${color}Setup reverse proxy${nocolor}"
-cp /home/centos/roboshop4-shell/frontend.conf /etc/nginx/default.d/roboshop.conf
-func_StatCheck
+cp /home/centos/roboshop4-shell/frontend.conf /etc/nginx/default.d/roboshop.conf    &>>${logfile}
+func_StatCheck $?
 
 echo -e "${color}Start Nginx${nocolor}"
-systemctl enable nginx
-systemctl restart nginx
-func_StatCheck
+systemctl enable nginx    &>>${logfile}
+systemctl restart nginx   &>>${logfile}
+func_StatCheck $?
