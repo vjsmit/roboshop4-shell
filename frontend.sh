058636470b5d042621e4535409608c4ca38f2409
smvijay <vj.smit97@gmail.com>
2024-03-27 03:56:20 -0400

    nodejs

--- frontend.sh
+++ frontend.sh
@@ -9,16 +9,16 @@ rm -rf /usr/share/nginx/html/*    &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Download frontend content${nocolor}"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip    &>>${logfile}
+curl -o /tmp/test.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip    &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Unzip frontend content${nocolor}"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip     &>>${logfile}
+unzip /tmp/test.zip     &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Setup reverse proxy${nocolor}"
-cp /home/centos/roboshop4-shell/frontend.conf /etc/nginx/default.d/roboshop.conf    &>>${logfile}
+cp /home/centos/roboshop4-shell/test.conf /etc/nginx/default.d/roboshop.conf    &>>${logfile}
 func_StatCheck $?
 
 echo -e "${color}Start Nginx${nocolor}"
